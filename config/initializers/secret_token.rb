c1c543e061c1eb92f4d65282a4c1b7b53b96c7f1
thienbao <thienbao@ubuntu.(none)>
2011-07-22 02:33:34 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KioskLocator::Application.config.secret_token = '5771e416d92590826607c870b8b6a14dbfa3338605bf388eb660feae442711e9856e6a76cecf730e9920a57d734b7ff9187444ad287bb844565f045dcbf4c74a'
